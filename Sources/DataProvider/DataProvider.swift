76cd2da4ed880c25942d9138d17ccef0fff40747
Christopher Alford <chrisvalford@me.com>
2020-11-09 21:15:03 +0100

    Initial commit

--- Sources/DataProvider/DataProvider.swift
+++ Sources/DataProvider/DataProvider.swift
@@ -1,3 +1,16 @@
-struct DataProvider {
-    var text = "Hello, World!"
+import SwiftUI
+
+@available(OSX 10.15, *)
+
+public class DelayedUpdater: ObservableObject {
+
+    @Published var value = 0
+
+    public init() {
+        for i in 1...100 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
 }
