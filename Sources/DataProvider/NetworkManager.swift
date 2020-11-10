e948bd370ef612c21a865c06fc0ce31cbae5cf05
Christopher Alford <chrisvalford@me.com>
2020-11-10 09:36:26 +0100

    updated init

--- Sources/DataProvider/NetworkManager.swift
+++ Sources/DataProvider/NetworkManager.swift
@@ -12,26 +12,24 @@ class NetworkManager: ObservableObject {
 
     var didChange = PassthroughSubject<NetworkManager, Never>()
 
-    var results = [OnlineOffer]() {
+    @Published public var results = [OnlineOffer]() {
         didSet {
             didChange.send(self)
         }
     }
 
-    init() {
+    public init?() {
         guard let url = URL(string: "https://marine.digital//INTERSHOP/static/WFS/Plus-NettoDE-Site/-/Plus-NettoDE/de_DE/Feed/os-angebote.json") else { return }
 
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
-            let offerResponse = try! JSONDecoder().decode(OnlineOfferResponse.self, from: data)
-            self.results = offerResponse.offers // If in an app should be on main thread!
+            do {
+                let offerResponse = try JSONDecoder().decode(OnlineOfferResponse.self, from: data)
+                self.results = offerResponse.offers // If in an app should be on main thread!
+            } catch {
+                return
+            }
         }.resume()
     }
 
-}
-
-
-struct SomeItem: Codable {
-    let id: UUID
-    let title: String
 }
